0810d51dda90d657c905e247d01ed71bbb6b63b8
Graham Smith <thegman@internetemails.net>
2018-06-19 22:08:32 +0100

    implemented api call

--- app/src/main/java/com/grayzemon/currencyconverter/MainActivity.java
+++ app/src/main/java/com/grayzemon/currencyconverter/MainActivity.java
@@ -8,6 +8,16 @@ import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.Spinner;
+import android.widget.TextView;
+
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.net.URL;
 import java.util.Arrays;
@@ -28,6 +38,12 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     private Spinner convertFrom;
     private Spinner convertTo;
     private List<String> currencyNames;
+    private URL url;
+    private String currencyFrom;
+    private String currencyTo;
+    private double rate;
+    private TextView numberConvertedAmount;
+    private Button buttonConvert;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,9 +51,10 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         setContentView(R.layout.activity_main);
         convertFrom = (Spinner) findViewById(R.id.spinner_convert_from);
         convertTo = (Spinner) findViewById(R.id.spinner_convert_to);
+        numberConvertedAmount = (TextView) findViewById(R.id.number_converted_amount);
 
-        Button convert = (Button) findViewById(R.id.button_convert) ;
-        convert.setOnClickListener(this);
+        buttonConvert = (Button) findViewById(R.id.button_convert);
+        buttonConvert.setOnClickListener(this);
 
         getCurrencyList();
 
@@ -45,10 +62,14 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 new ArrayAdapter<>(this,R.layout.support_simple_spinner_dropdown_item,currencyNames);
         convertFrom.setAdapter(currencies);
         convertTo.setAdapter(currencies);
+        convertFrom.setSelection(9);
+        convertTo.setSelection(8);
     }
 
     private void getCurrencyList() {
-        final String currencyCodeList = "AUD,BGN,BRL,CAD,CHF,CNY,CZK,DKK,GBP,HKD,HRK,HUF,IDR,ILS,INR,ISK,JPY,KRW,MXN,MYR,NOK,NZD,PHP,PLN,RON,RUB,SEK,SGD,THB,TRY,USD,ZAR";
+        final String currencyCodeList =
+                "AUD,BGN,BRL,CAD,CHF,CNY,CZK,DKK,GBP,EUR,HKD,HRK,HUF,IDR,ILS," +
+                "INR,ISK,JPY,KRW,MXN,MYR,NOK,NZD,PHP,PLN,RON,RUB,SEK,SGD,THB,TRY,USD,ZAR";
         final List<String> currencyCodes = Arrays.asList(currencyCodeList.split(","));
 
         Function<String,String> mapCode2Desc = currencyCode -> Currency.getInstance(currencyCode).getDisplayName() + " " + currencyCode;
@@ -61,17 +82,26 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     @Override
     public void onClick(View v) {
         Log.d(TAG,"onClick: Button pressed");
+        buttonConvert.setEnabled(false);
+        getCurrencyCodesFromDropdowns();
+        buildURL();
+        //showSnackBar(url.toString());
+        getCurrencyRate();
+    }
 
+    private void getCurrencyCodesFromDropdowns() {
         int selectedPositionFrom = convertFrom.getSelectedItemPosition();
         int selectedPositionTo = convertTo.getSelectedItemPosition();
         String selectedCurrencyFrom = currencyNames.get(selectedPositionFrom);
         String selectedCurrencyTo = currencyNames.get(selectedPositionTo);
-        String currencyFrom = selectedCurrencyFrom.substring(
+        currencyFrom = selectedCurrencyFrom.substring(
                 selectedCurrencyFrom.length() - 3,selectedCurrencyFrom.length());
-        String currencyTo = selectedCurrencyTo.substring(
+        currencyTo = selectedCurrencyTo.substring(
                 selectedCurrencyTo.length() - 3,selectedCurrencyTo.length());
+    }
 
-        URL url = new HttpUrl.Builder()
+    private void buildURL() {
+        url = new HttpUrl.Builder()
                 .scheme("https")
                 .host("exchangeratesapi.io")
                 .addPathSegments("api/latest")
@@ -79,7 +109,38 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 .addQueryParameter("symbols", currencyTo)
                 .build().url();
         Log.d(TAG, url.toString());
+    }
+
+    private void showSnackBar(String text) {
         View view = findViewById(R.id.constraint_main);
-        Snackbar.make(view, "URL=" + url.toString(),Snackbar.LENGTH_SHORT).show();
+        Snackbar.make(view, text,Snackbar.LENGTH_SHORT).show();
+    }
+
+    public void getCurrencyRate(){
+        RequestQueue queue = Volley.newRequestQueue(this);
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(
+                url.toString(),
+                null,
+                new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        try {
+                            buttonConvert.setEnabled(true);
+                            rate = response.getJSONObject("rates").getDouble(currencyTo);
+                            Log.d(TAG,"Converted=" + rate);
+                            numberConvertedAmount.setText(String.valueOf(rate));
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                            showSnackBar(e.getMessage());
+                        }
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                buttonConvert.setEnabled(true);
+                Log.d(TAG,error.getMessage());
+            }
+        });
+        queue.add(jsonObjectRequest);
     }
 }
